9324ef3433c6c9ebcc5477f5fad85db5897f66c5
kawanet <u-suke@kawa.net>
2021-01-21 14:28:21 +0900

    REPEAT=1000 on IE11

--- test/99.benchmark.ts
+++ test/99.benchmark.ts
@@ -7,7 +7,8 @@ import {MAKURANOSOSHI} from "./utils/sample-text";
 const TITLE = __filename.split("/").pop();
 
 const isBrowser = ("undefined" !== typeof window);
-const REPEAT = process.env.REPEAT || (isBrowser ? 10000 : 10000);
+const isLegacy = ("function" !== typeof TextEncoder);
+const REPEAT = process.env.REPEAT || (isBrowser ? (isLegacy ? 1000 : 10000) : 10000);
 const SLEEP = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 const stringToArray = (str: string) => [].map.call(unescape(encodeURIComponent(str)), (c: string) => c.charCodeAt(0))
 
